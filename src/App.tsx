0154d19966311855cf6dafdea4acef8961c28bf7
Thomas Bisson <bisson.th@gmail.com>
2024-11-20 20:45:41 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import Dashboard from './dashboard/Dashboard';
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
